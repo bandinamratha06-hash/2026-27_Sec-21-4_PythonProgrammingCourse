0b199fe224de0ee0bebbad2146aba2b2c57d047d
bandinamratha06-hash <bandinamratha06@gmail.com>
2026-09-04 15:30:41 +0530

    Add files via upload

---
+++ lab 5.py
@@ -0,0 +1,4 @@
+a=str(input("enter a:"))
+b=str(input("enter b:"))
+a,b=b,a
+print(a,b)
---
+++ lab4.py
@@ -0,0 +1,3 @@
+celcius=int(input("enter the celcius temperature"))
+fahrenheit=(celcius*9/5+32)
+print(fahrenheit)
