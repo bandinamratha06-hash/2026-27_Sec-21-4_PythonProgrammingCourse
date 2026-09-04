21cd9ae698e499d2b72924dc4616af2dea1cf707
bandinamratha06-hash <bandinamratha06@gmail.com>
2026-09-04 14:21:26 +0530

    Add files via upload

---
+++ identiify operaters.py
@@ -0,0 +1,12 @@
+#a=[1,2,3]
+#b=a
+#result=b is a
+#print("resultof",b,"is",a,"is;",result)
+#n=[1,2,3]
+#o=[1,2,3]
+#result=n is not o
+#print("result of",o,"is not",n,"is not;",result)
+s=[0,6,1,6]
+p=s
+result=s is p
+print("result of",p,"is not",s,"is not;",result)
