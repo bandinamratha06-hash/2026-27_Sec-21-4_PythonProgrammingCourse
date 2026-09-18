8b3b0e2e3eaf2e698ff2cc7679c1213c75eb7670
bandinamratha06-hash <bandinamratha06@gmail.com>
2026-09-18 14:23:29 +0530

    Delete lab 6.py

--- lab 6.py
+++
@@ -1,5 +0,0 @@
-p=int(input("enter the principle in rupee"))
-r=int(input("enter the rate intrest in percentage "))
-t=int(input("enter the time in years"))
-simple_intrest=p*r*t/100
-print(simple_intrest)        
