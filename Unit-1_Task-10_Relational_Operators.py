d34927abda666bf4b98a9d7dfb2e32b9ee6e5bd6
bandinamratha06-hash <bandinamratha06@gmail.com>
2026-09-25 14:50:02 +0530

    Add files via upload

---
+++ Unit-1_Task-10_Relational_Operators.py
@@ -0,0 +1,30 @@
+Python 3.14.7 (tags/v3.14.7:823f032, Aug  5 2026, 10:51:32) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/Bandi Namratha/Desktop/python lab/Unit-2A_Task-1_Valid_Triangle_Angles.py
+enter the angle145
+enter the angle245
+enter the angle390
+valid triangle(angle=45,45,90)
+>>> #Python comparision operators excercise
+... a=10
+... b=5
+... result=a>b #greater than comparision operator
+... print("result of",a,">",b,"is:",result)
+... 
+... result=a<b #less than comparision operator
+... print("result of",a,"<",b,"is:",result)
+... 
+... """a=15
+... b=15
+... result=a==b #equal for comparision operator 
+... print("result of",a,"==",b,"is :",result)"""
+... 
+... """a=10
+... b=15
+... result=a!=b #not equal for comparision operator
+... print("result of",a,"!=",b,"is :",result)"""
+... 
+... a=15
+... b=10
+... result=a>=b #greater than equal to comparision operator
