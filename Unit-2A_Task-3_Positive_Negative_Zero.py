f959e68e59b96d8ea93e1e981bebadd7294ede1e
bandinamratha06-hash <bandinamratha06@gmail.com>
2026-09-25 16:21:31 +0530

    Add files via upload

---
+++ Unit-2A_Task-3_Positive_Negative_Zero.py
@@ -0,0 +1,8 @@
+#check weather a number is positive or negitive or zero
+a=int(input("enter the number"))
+if a>0:
+    print(f"positive number(a={a})")
+elif a<0:
+    print(f"negitive number(a={a})")
+else:
+    print(f"zero (a={a})")
