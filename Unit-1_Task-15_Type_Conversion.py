e33d40317cebc85324ee3799b82586a810808911
bandinamratha06-hash <bandinamratha06@gmail.com>
2026-09-25 20:36:38 +0530

    Add files via upload

---
+++ Unit-1_Task-15_Type_Conversion.py
@@ -0,0 +1,21 @@
+# Demonstrating type conversion in Python
+# String to Integer
+a = int(input("enter a num1"))
+b = int(a)
+print("String to Integer:", b, type(b))
+# Integer to Float
+c = int(input("enter a num2"))
+d = float(c)
+print("Integer to Float:", d, type(d))
+# Float to Integer
+e = float(input("enter a flout num"))
+f = int(e)
+print("Float to Integer:", f, type(f))
+# Integer to String
+g = int(input("enter a num"))
+h = str(g)
+print("Integer to String:", h, type(h))
+# Float to String
+i = float(input("enter a num"))
+j = str(i)
+print("Float to String:", j, type(j))
